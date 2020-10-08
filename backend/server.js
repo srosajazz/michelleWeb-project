8154f71459b967690fd5b0b1d43d5e994909ec94
Sergio Rosa <scarlosjazz@gmail.com>
2020-10-08 08:40:24 -0400

    chore: Modeling Data - MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 // dotenv
 dotenv.config();
+
+// DB connection
+connectDB();
 
 const app = express();
 
@@ -26,5 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
